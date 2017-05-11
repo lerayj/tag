2b79d91040b81b4b62e9928b8d10c64e2f85853f
Leray Julien <leray.jul@gmail.com>
2017-05-11 17:17:31 +0200

    V0.2

--- app/index.js
+++ app/index.js
@@ -3,6 +3,7 @@ import Cookies from 'js-cookie';
 import firebase from 'firebase';
 import uuid from 'uuid/V1';
 import email_validator from 'email-validator';
+import psl from 'psl';
 
 // Initialize Firebase
 var config = {
@@ -17,11 +18,13 @@ firebase.initializeApp(config);
 
 var database = firebase.database();
 
-var websiteKey = window.location.hostname.replace(/\./g, "_");
+//var websiteKey = window.location.hostname.replace(/\./g, "_");
+console.log("host: ", window.location.host, " psl: ", psl.parse(window.location.host).domain, "real psl: ", psl);
+var websiteKey = psl.parse(window.location.host).domain.replace(/\./g, "_");
 console.log("websiteKey: ", websiteKey);
 
 //TEST
-websiteKey = "toto_com";
+//websiteKey = "toto_com";
 
 
 var sessionId = getCookieSession();
@@ -30,7 +33,8 @@ getConfig(websiteKey).then((config) => {
     console.log("CONFIG: ", config);
     saveMail(config, websiteKey, sessionId);
     confirmSoldBasket(config, websiteKey, sessionId);
-    window.onload = saveBasket(config, websiteKey, sessionId);
+    //    window.onload = saveBasket(config, websiteKey, sessionId);
+    saveBasket(config, websiteKey, sessionId);
 });
 
 
@@ -90,24 +94,78 @@ function saveMail(config, websiteId, sessionId){
 //TODO: Faire des règles plus générique sur les urls
 //TODO: Faire la config approprié au test
 function saveBasket(config, websiteId, sessionId){
-    if(config.basket.url == window.location.pathname.substr(1)){
+    console.log("config url: ", config.basket.url, " current url: ", window.location.pathname.substr(1));
+    if(config.basket.url == window.location.pathname.substr(1) || 
+        config.basket.url + '/' == window.location.pathname.substr(1) ){
         var total = Sizzle(config.basket.total)[0].textContent,
             linesProducts = Sizzle(config.basket.container + " " + config.basket.product_line),
             updates = {},
             basket = 'websites/' + websiteId + '/sessions/' + sessionId + '/basket';
-
+            console.log("linesProducts: ", linesProducts, " selector: ", config.basket.container + " " + config.basket.product_line);
         linesProducts.forEach((line, idx) => {
-            var product_id = Sizzle(config.basket.product_id)[idx].textContent;
-            updates[basket + '/products/' + product_id + '/product_name'] = Sizzle(config.basket.product_name)[0].textContent;
-            updates[basket + '/products/' + product_id + '/product_price'] = Sizzle(config.basket.product_price)[0].textContent;
-            updates[basket + '/products/' + product_id + '/product_quantity'] = Sizzle(config.basket.product_quantity)[0].textContent;
+            var product_id = attributeValue(config, 'product_id', line);
+            console.log("Product_id: ", product_id);
+
+            var product_name = attributeValue(config, 'product_name', line);
+            console.log("Product name: ", product_name);
+            updates[basket + '/products/' + product_id + '/product_name'] = product_name;
+
+            var product_price = attributeValue(config, 'product_price', line);
+            console.log("Product price: ", product_price);
+            updates[basket + '/products/' + product_id + '/product_price'] = product_price;
+
+            var product_quantity = attributeValue(config, 'product_quantity', line);
+            console.log("Product quantity: ", product_quantity);
+            updates[basket + '/products/' + product_id + '/product_quantity'] = product_quantity;
+
+            //console.log("product name: ", Sizzle(config.basket.product_name, line)[0].textContent)
+            //updates[basket + '/products/' + product_id + '/product_name'] = Sizzle(config.basket.product_name, line)[0].textContent.replace(/(\r\n\t|\n|\r\t)/gm,"").replace(/^\s+|\s+$/g, "");
+            
+            //console.log("product price: ", Sizzle(config.basket.product_price)[0].textContent)
+            //updates[basket + '/products/' + product_id + '/product_price'] = Sizzle(config.basket.product_price, line)[0].textContent.replace(/(\r\n|\n|\r)/gm,"").replace(/^\s+|\s+$/g, "");
+            
+            //var product_quantity = attributeValue(config, 'product_quantity', idx).replace(/(\r\n\t|\n|\r\t)/gm,"").replace(/^\s+|\s+$/g, "");
+            //console.log("product_quantity: ", product_quantity);
+            //updates[basket + '/products/' + product_id + '/product_quantity'] = product_quantity;
             
         });
         console.log("Updates: ", updates);
-        updates[basket + '/total'] = total;
+        updates[basket + '/total'] = total.replace(/(\r\n\t|\n|\r\t)/gm,"").replace(/^\s+|\s+$/g, "");
         firebase.database().ref().update(updates);
     }
 
+}
+
+//Si l'id_product est un attribut: @selector:attribut (eg RdC: @.cartProduct:id)
+function attributeValue(config, configKeyName, line){
+    //console.log("IN ATTRIBUTE VALUE:", config.basket[configKeyName]);
+    var configRule = config.basket[configKeyName],
+        result = null;
+    if(configRule[0] == '@'){
+        //console.log("prod id: ", config.basket.product_id, " sub prod: ", config.basket.product_id.substr(1), " split prod: ", config.basket.product_id.substr(1).split(':'));
+        var options = configRule.substr(1).split(':');
+        //console.log("@ selecteur: ", options);
+        
+        //console.log("options[0]: ", options[0]);
+        //console.log("TEST SELECT: ", Sizzle(options[0], line));
+        var selection = Sizzle(options[0], line);
+        if(selection.length == 0 && Sizzle.matchesSelector(line, options[0])){
+            //console.log("line: ", line, " option: ", options[0] ,"NEW: ",line[options[0]]);
+            //console.log("SIZZLE TEST: ", Sizzle.matchesSelector(line, options[0]));
+            //console.log("PLEASE: ", line[options[1]])
+            result = line[options[1]];
+        } else{
+            var value = Sizzle(options[0], line)[0];
+
+            console.log(" line: ", line, " selecteur: ", options[0], "value: ", value , "result: ", value[options[1]], );
+            result = value[options[1]];  
+        }
+
+    } else{
+        //console.log("Regular: ", Sizzle(configRule, line)[0], " focus: ", Sizzle(configRule, line));
+        result = Sizzle(configRule, line)[0].textContent;
+    }
+    return result.replace(/(\r\n\t|\n|\r\t)/gm,"").replace(/^\s+|\s+$/g, "");
 }
 
 //TODO: For SAP navigation without HashChange
